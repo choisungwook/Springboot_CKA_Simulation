64252eaab6e900b9fc57e2ea21d0b28e66b0ea68
choisungwook <kgg1959@naver.com>
2020-10-23 21:27:43 +0900

    Add webpack.config.js

---
+++ src/main/resources/webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './client/index.js',
+  optimization: {
+    minimize: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./client/index.html",
+      minify: false
+    }),
+    new MiniCssExtractPlugin({
+      filename: "xterm.css",
+    }),
+  ],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        use: [
+          "html-loader?minimize=false",
+        ]
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+};
