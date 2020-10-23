0c77df39d04853d3d7571556de6a7abbd2ae466b
choisungwook <kgg1959@naver.com>
2020-10-23 21:08:43 +0900

    Delete ssh code

--- src/main/java/ssh2/springboot_ssh_client/websocket/WebsocketHandlerImpl.java
+++ src/main/java/ssh2/springboot_ssh_client/websocket/WebsocketHandlerImpl.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.*;
-import ssh2.springboot_ssh_client.sshclient.SSHClientHandlerImpl;
 
 @Component
 @RequiredArgsConstructor
