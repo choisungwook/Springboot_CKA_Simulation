af241b3f306d750fce24ccb6a61d6af1126103b7
choisungwook <kgg1959@naver.com>
2020-10-24 13:45:50 +0900

    Change setter 삭제

--- src/main/java/ssh2/springboot_ssh_client/controller/question/dto/Request_findById_question_dto.java
+++ src/main/java/ssh2/springboot_ssh_client/controller/question/dto/Request_findById_question_dto.java
@@ -3,10 +3,8 @@ package ssh2.springboot_ssh_client.controller.question.dto;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
-@Setter
 @NoArgsConstructor
 public class Request_findById_question_dto {
     private Long id;
