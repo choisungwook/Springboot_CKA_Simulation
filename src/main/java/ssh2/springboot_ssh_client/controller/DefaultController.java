bf58bb1e1e3e36f07870a02ee7f250d7472d69dd
choisungwook <kgg1959@naver.com>
2020-10-24 16:24:39 +0900

    Add 모든문제조회 Controller, html 연동

--- src/main/java/ssh2/springboot_ssh_client/controller/DefaultController.java
+++ src/main/java/ssh2/springboot_ssh_client/controller/DefaultController.java
@@ -1,12 +1,20 @@
 package ssh2.springboot_ssh_client.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ssh2.springboot_ssh_client.question.service.Question_rest_service;
 
 @Controller
+@RequiredArgsConstructor
 public class DefaultController {
+    private final Question_rest_service question_rest_service;
+
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("questions", question_rest_service.findAll());
+
         return "index";
     }
 }
