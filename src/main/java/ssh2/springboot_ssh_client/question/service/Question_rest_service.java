3b7bfb66a1b9680f35c4a7664f1396b2f5461acb
choisungwook <kgg1959@naver.com>
2020-10-24 13:27:15 +0900

    Change restAPI 단일문제 조회

--- src/main/java/ssh2/springboot_ssh_client/question/service/Question_rest_service.java
+++ src/main/java/ssh2/springboot_ssh_client/question/service/Question_rest_service.java
@@ -3,6 +3,7 @@ package ssh2.springboot_ssh_client.question.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ssh2.springboot_ssh_client.controller.question.dto.Request_create_question_dto;
+import ssh2.springboot_ssh_client.controller.question.dto.Requset_findById_question_dto;
 import ssh2.springboot_ssh_client.controller.question.dto.Response_findById_question_dto;
 import ssh2.springboot_ssh_client.question.QuestionDomain;
 import ssh2.springboot_ssh_client.question.QuestionRepository;
@@ -12,8 +13,8 @@ import ssh2.springboot_ssh_client.question.QuestionRepository;
 public class Question_rest_service {
     private final QuestionRepository questionRepository;
 
-    public Response_findById_question_dto findById(Long id){
-        QuestionDomain find_question = questionRepository.findById(id)
+    public Response_findById_question_dto findById(Requset_findById_question_dto request){
+        QuestionDomain find_question = questionRepository.findById(request.getId())
                 .orElseThrow(
                         () -> new IllegalStateException("존재하지 않은 Id")
                 );
