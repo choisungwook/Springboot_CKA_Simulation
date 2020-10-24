294e4b7aa0f39d41ada1d15f62f369f01c340ecc
choisungwook <kgg1959@naver.com>
2020-10-24 12:58:48 +0900

    Add restAPI 문제생성 서비스

---
+++ src/main/java/ssh2/springboot_ssh_client/question/service/Question_rest_service.java
@@ -0,0 +1,32 @@
+package ssh2.springboot_ssh_client.question.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ssh2.springboot_ssh_client.controller.question.dto.Request_create_question_dto;
+import ssh2.springboot_ssh_client.controller.question.dto.Response_findById_question_dto;
+import ssh2.springboot_ssh_client.question.QuestionDomain;
+import ssh2.springboot_ssh_client.question.QuestionRepository;
+
+@Service
+@RequiredArgsConstructor
+public class Question_rest_service {
+    private final QuestionRepository questionRepository;
+
+    public Response_findById_question_dto findById(Long id){
+        QuestionDomain find_question = questionRepository.findById(id)
+                .orElseThrow(
+                        () -> new IllegalStateException("존재하지 않은 Id")
+                );
+
+        return Response_findById_question_dto.builder()
+                .id(find_question.getId())
+                .content(find_question.getContent())
+                .build();
+
+    }
+
+    public Long save(Request_create_question_dto request){
+        Long saveId = questionRepository.save(request.toEntity()).getId();
+        return saveId;
+    }
+}
