c8af3cfe7243ef116301505f07254db944ef1955
choisungwook <kgg1959@naver.com>
2020-10-24 12:59:01 +0900

    Add restAPI 문제생성 Test

---
+++ src/test/java/ssh2/springboot_ssh_client/question/service/Question_rest_serviceTest.java
@@ -0,0 +1,59 @@
+package ssh2.springboot_ssh_client.question.service;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import ssh2.springboot_ssh_client.controller.question.dto.Request_create_question_dto;
+import ssh2.springboot_ssh_client.question.QuestionDomain;
+import ssh2.springboot_ssh_client.question.QuestionRepository;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class Question_rest_serviceTest {
+    @Autowired Question_rest_service question_rest_service;
+    @Autowired QuestionRepository questionRepository;
+
+    @Test
+    public void 단일문제조회(){
+
+    }
+
+    @Test
+    public void 문제생성(){
+        Request_create_question_dto question_dto1 = Request_create_question_dto.builder()
+                .content("111")
+                .build();
+        Request_create_question_dto question_dto2 = Request_create_question_dto.builder()
+                .content("222")
+                .build();
+
+        create_question(question_dto1);
+        create_question(question_dto2);
+    }
+
+    /***
+     * 문제 생성
+     * @param request dto
+     * @return 생성한 문제 엔티티
+     */
+    private QuestionDomain create_question(Request_create_question_dto request){
+        //given
+        QuestionDomain new_question = request.toEntity();
+
+        //when
+        Long saveId = question_rest_service.save(request);
+
+        //then
+        QuestionDomain find_question = questionRepository.findById(saveId)
+                .orElseThrow(
+                        () -> new IllegalStateException("존재하지 않은 Id")
+                );
+        Assertions.assertThat(find_question.getContent()).isEqualTo(new_question.getContent());
+
+        return find_question;
+    }
+
+}
