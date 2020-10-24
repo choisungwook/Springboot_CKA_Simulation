538ffe68d9ebe266e0a3f5f3966c09adb6afc70e
choisungwook <kgg1959@naver.com>
2020-10-24 17:37:11 +0900

    Add 문제보기 이벤트

--- src/main/resources/static/assets/js/question.js
+++ src/main/resources/static/assets/js/question.js
@@ -1,2 +1,27 @@
+// index.html 로드 이후에 1번 문제 요청
+window.onload=function(){
+    $.ajax({
+        type: 'GET',
+        url: '/api/v1/question/' + 1,
+        dataType: 'json',
+        contentType: 'application/json; charset=utf-8',
+    }).done(function(data) {
+        document.getElementById("question_content").innerHTML = data['content'];
+    }).fail(function(){
+    });
+}
 
-// select box
+
+// select box 이벤트 설정
+let question_selectbox = document.querySelector("#question_select");
+    question_selectbox.addEventListener('change', function(event){
+        $.ajax({
+            type: 'GET',
+            url: '/api/v1/question/' + this.value,
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+        }).done(function(data) {
+            document.getElementById("question_content").innerHTML = data['content'];
+        }).fail(function(){
+        });
+});
