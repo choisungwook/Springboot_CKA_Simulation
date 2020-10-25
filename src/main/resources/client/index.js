51216d0686ec53f98baf986df9650219979b78cf
choisungwook <kgg1959@naver.com>
2020-10-25 10:27:57 +0900

    Add websocket 연결버튼

--- src/main/resources/client/index.js
+++ src/main/resources/client/index.js
@@ -1,18 +1,38 @@
 import { Terminal } from 'xterm';
 import { AttachAddon } from 'xterm-addon-attach';
-import './xterm.css'
 
-const term = new Terminal();
-const socket = new WebSocket('ws://localhost:8010');
-const attachAddon = new AttachAddon(socket);
+let connected = false;
 
-// Attach the socket to term
-socket.onopen = function() {
-    term.loadAddon(attachAddon);    
-};
+// 연결 이벤트 설정
+let btn = document.querySelector("#connect_btn");
+    if(term){
+        term.destroy();
+    }
+    
+    const term = new Terminal();
+    term.open(document.getElementById('xterm-container'));
 
-socket.onerror = function(e) {
-    console.log(e);
-};
+    btn.addEventListener('click', function(event){
+        if(connected == false){
+            let host = document.getElementById("host").value;
+            let port = document.getElementById("port").value;
+            console.log(host + ":" + port);
 
-term.open(document.getElementById('xterm-container'));
+            // const term = new Terminal();
+            const socket = new WebSocket('ws://' + host + ":" + port);
+            const attachAddon = new AttachAddon(socket);
+
+            // Attach the socket to term
+            socket.onopen = function() {
+                term.loadAddon(attachAddon);
+                connected = true;
+            };
+
+            socket.onerror = function(e) {
+                console.log(e);
+                alert("연결주소를 확인하고 다시 입력해주세요");
+            };
+
+            // term.open(document.getElementById('xterm-container'));
+        }
+});
