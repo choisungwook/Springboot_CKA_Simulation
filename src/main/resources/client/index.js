88c144e03941d38bd1984b0442d649b5b50a2327
choisungwook <kgg1959@naver.com>
2020-10-25 10:45:57 +0900

    Chane xtermjs 터미널 연결

--- src/main/resources/client/index.js
+++ src/main/resources/client/index.js
@@ -2,37 +2,32 @@ import { Terminal } from 'xterm';
 import { AttachAddon } from 'xterm-addon-attach';
 
 let connected = false;
-
-// 연결 이벤트 설정
-let btn = document.querySelector("#connect_btn");
-    if(term){
-        term.destroy();
-    }
+let websock = null;
     
-    const term = new Terminal();
-    term.open(document.getElementById('xterm-container'));
+const term = new Terminal();
+term.open(document.getElementById('xterm-container'));
 
-    btn.addEventListener('click', function(event){
-        if(connected == false){
-            let host = document.getElementById("host").value;
-            let port = document.getElementById("port").value;
-            console.log(host + ":" + port);
+let btn = document.querySelector("#connect_btn");
+btn.addEventListener('click', function(event){
+    if(websock == null){
+        let host = document.getElementById("host").value;
+        let port = document.getElementById("port").value;
 
-            // const term = new Terminal();
-            const socket = new WebSocket('ws://' + host + ":" + port);
-            const attachAddon = new AttachAddon(socket);
+        // const term = new Terminal();
+        websock = new WebSocket('ws://' + host + ":" + port);
 
-            // Attach the socket to term
-            socket.onopen = function() {
-                term.loadAddon(attachAddon);
-                connected = true;
-            };
+        // Attach the socket to term
+        websock.onopen = function() {
+            const attachAddon = new AttachAddon(websock);
+            term.loadAddon(attachAddon);
+            connected = true;
+        };
 
-            socket.onerror = function(e) {
-                console.log(e);
-                alert("연결주소를 확인하고 다시 입력해주세요");
-            };
+        websock.onerror = function(e) {
+            alert("연결주소를 확인하고 다시 입력해주세요");
+            websock = null;
+        };
 
-            // term.open(document.getElementById('xterm-container'));
-        }
+        // term.open(document.getElementById('xterm-container'));
+    }
 });
